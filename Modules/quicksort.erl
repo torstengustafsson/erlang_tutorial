5fb419416c3399a5ef52e4291fd8ee37bdb95ba1
torstengustafsson <torstengustafsson1992@gmail.com>
2017-11-13 16:41:56 +0100

    added quicksort module

---
+++ Modules/quicksort.erl
@@ -0,0 +1,25 @@
+-module(quicksort).
+
+-export([quicksort/1,
+        lc_quicksort/1]).
+
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+  {Smaller, Larger} = partition(Pivot, Rest, [], []),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+partition(_, [], Smaller, Larger) -> {Smaller, Larger};
+partition(Pivot, [H|T], Smaller, Larger) ->
+  if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+     H >  Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+  end.
+
+% quicksort with list comprehensions
+% easier to read, but has to traverse the list twice
+% to partition the list in smaller and larger parts
+lc_quicksort([]) -> [];
+lc_quicksort([Pivot|Rest]) ->
+  lc_quicksort([Smaller || Smaller <- Rest, Smaller =< Pivot])
+  ++ [Pivot] ++
+  lc_quicksort([Larger || Larger <- Rest, Larger > Pivot]).
+
